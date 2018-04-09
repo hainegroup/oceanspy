7c8710243cb81e920b53dfee49b37a0b22d11e72
Mattia Almansi <mattia.almansi@jhu.edu>
2018-04-09 15:52:57 -0400

    Release 0.0.2: generate_ds_grid

--- oceanspy/__init__.py
+++ oceanspy/__init__.py
@@ -6,4 +6,4 @@ __author__ = """Mattia Almansi"""
 __email__ = 'mattia.almansi@jhu.edu'
 __version__ = '0.0.1'
 
-from .autogenerate import generate_grid_ds
+from .autogenerate import generate_ds_grid
