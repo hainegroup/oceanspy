e2171f6ccff6a41a98302b18ce4f795542e6f4f4
Mattia Almansi <mattia.almansi@jhu.edu>
2018-04-09 17:21:54 -0400

    add packages imported

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['numpy', 'pandas', 'xarray', 'xgcm', 'time',]
 
 setup_requirements = ['pytest-runner', ]
 
