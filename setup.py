17964409f872b0b8cd7bf9ea02b19d3eed622029
Mattia Almansi <mattia.almansi@jhu.edu>
2018-04-09 18:21:07 -0400

    review requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'pandas', 'xarray', 'xgcm', 'time',]
+requirements = ['xarray', 'dask', 'cartopy', 'bottleneck', 'netCDF4', 'xgcm', ]
 
 setup_requirements = ['pytest-runner', ]
 
