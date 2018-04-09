ffbeba11fbb4b004414833c8cda3f7183d624e62
Mattia Almansi <mattia.almansi@jhu.edu>
2018-04-09 15:06:30 -0400

    fix version

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/malmans2/oceanspy',
-    version='0.2.0',
+    version='0.0.0',
     zip_safe=False,
 )
