ea8255b797783d322aa87d425ce726244b8c14bf
Mattia Almansi <mattia.almansi@jhu.edu>
2018-04-09 12:13:51 -0400

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -91,13 +91,16 @@ html_theme = 'sphinx_rtd_theme'
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'logo_only': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "_static/oceanspy_logo_white.png"
 
 # -- Options for HTMLHelp output ---------------------------------------
 
