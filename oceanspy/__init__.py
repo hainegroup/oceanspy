38fcfd2f95a7628ea466bc2b4354b383634e6249
Mattia Almansi <mattia.almansi@jhu.edu>
2018-04-11 18:03:22 -0400

    Bump version: 0.0.2 → 0.0.3

--- oceanspy/__init__.py
+++ oceanspy/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Mattia Almansi"""
 __email__ = 'mattia.almansi@jhu.edu'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from .autogenerate import generate_ds_grid
 from .oceanspy import *
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/malmans2/oceanspy',
-    version='0.0.2',
+    version='0.0.3',
     zip_safe=False,
 )
