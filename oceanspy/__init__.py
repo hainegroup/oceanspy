ba5c830af1d985d6278df990fa110c56baca8448
malmans2 <mattia.almansi@jhu.edu>
2018-05-21 21:11:22 +0000

    ready for upload

--- oceanspy/__init__.py
+++ oceanspy/__init__.py
@@ -6,6 +6,5 @@ __author__ = """Mattia Almansi"""
 __email__ = 'mattia.almansi@jhu.edu'
 __version__ = '0.0.4'
 
-from ._useful_funcs import plot_mercator, disp_variables
 from ._autogenerate import generate_ds
 from ._cutout import Cutout
