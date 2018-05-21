ba1d439d4754e61931e70abd657d00078828039d
Mattia Almansi <mattia.almansi@jhu.edu>
2018-05-21 19:00:39 -0400

    Do not test

--- oceanspy/tests/test_oceanspy.py
+++ oceanspy/tests/test_oceanspy.py
@@ -3,6 +3,7 @@
 
 """Tests for `oceanspy` package."""
 
+"""
 import pytest
 
 
@@ -23,3 +24,4 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+"""
