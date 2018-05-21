8e9fc180b3f0c9a7d8b26f42570da490176a17b3
malmans2 <mattia.almansi@jhu.edu>
2018-05-21 17:18:42 +0000

    Cutout all set

--- oceanspy/__init__.py
+++ oceanspy/__init__.py
@@ -6,5 +6,6 @@ __author__ = """Mattia Almansi"""
 __email__ = 'mattia.almansi@jhu.edu'
 __version__ = '0.0.4'
 
-from .autogenerate import generate_ds
-from .subsample import Subsample
+from ._useful_funcs import plot_mercator, disp_variables
+from ._autogenerate import generate_ds
+from ._cutout import Cutout
---
+++ oceanspy/_autogenerate.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import xgcm as xgcm
+import time
+
+from ._useful_funcs import *
+
+def generate_ds(cropped   = False):
+    """
+    Generate a Dataset from NetCDFs.
+    This function cointains hard-coded parameters specific for SciServer.
+    
+    exp_ASR is the only dataset currently available.
+    
+    Parameters
+    ----------
+    cropped: bool
+            If True include variables to close the heat/salt budget. Budget's variables
+            have been cropped and have a smaller domain (lonRange:[69, 72]; latRange:[-13, -22]). 
+            Thus, also the regular variables will be cropped and only one Dataset is created.
+               
+    Returns
+    -------
+    ds: xarray.Dataset
+       Dataset with all the available variables
+    """
+    
+    # Check parameters
+    if not isinstance(cropped, bool)  : raise RuntimeError("'cropped' needs to be a Boolean")
+          
+    # Hello
+    start_time = time.time()
+    print('Opening dataset:',end=' ')
+    
+    # Import grid and fields separately, then merge
+    gridpath = '/home/idies/workspace/OceanCirculation/exp_ASR/grid_glued.nc'
+    fldspath = '/home/idies/workspace/OceanCirculation/exp_ASR/result_*/output_glued/*.*_glued.nc'
+    croppath = '/home/idies/workspace/OceanCirculation/exp_ASR/result_*/output_glued/cropped/*.*_glued.nc'
+    gridset = xr.open_dataset(gridpath,
+                              drop_variables = ['XU','YU','XV','YV','RC','RF','RU','RL'])
+    fldsset = xr.open_mfdataset(fldspath,
+                                concat_dim     = 'T',
+                                drop_variables = ['diag_levels','iter'])
+    ds = xr.merge([gridset, fldsset])
+    
+    # Create horizontal vectors (remove zeros due to exch2)
+    ds['X'].values   = ds.XC.where((ds.XC!=0) & (ds.YC!=0)).mean(dim='Y',   skipna=True)
+    ds['Xp1'].values = ds.XG.where((ds.XG!=0) & (ds.YG!=0)).mean(dim='Yp1', skipna=True)
+    ds['Y'].values   = ds.YC.where((ds.XC!=0) & (ds.YC!=0)).mean(dim='X',   skipna=True)
+    ds['Yp1'].values = ds.YG.where((ds.XG!=0) & (ds.YG!=0)).mean(dim='Xp1', skipna=True)
+    ds = ds.drop(['XC','YC','XG','YG'])
+    
+    # Negative dr in order to be consistent with upward z axis
+    ds['drC'].values = -ds['drC'].values
+    ds['drF'].values = -ds['drF'].values
+       
+    # Read cropped files and crop ds
+    if cropped:
+        cropset = xr.open_mfdataset(croppath,
+                                    concat_dim     = 'T',
+                                    drop_variables = ['diag_levels','iter'])
+        cropset = cropset.rename({'Zld000216': 'Zl'})
+        ds = ds.isel(X   = slice(min(cropset['X'].values).astype(int)-1, 
+                                 max(cropset['X'].values).astype(int)),
+                     Xp1 = slice(min(cropset['Xp1'].values).astype(int)-1, 
+                                 max(cropset['Xp1'].values).astype(int)),
+                     Y   = slice(min(cropset['Y'].values).astype(int)-1, 
+                                 max(cropset['Y'].values).astype(int)),
+                     Yp1 = slice(min(cropset['Yp1'].values).astype(int)-1,
+                                 max(cropset['Yp1'].values).astype(int)))
+        ds = ds.isel(Z         = slice(0,cropset['Zmd000216'].size),
+                     Zl        = slice(0,cropset['Zmd000216'].size),
+                     Zmd000216 = slice(0,cropset['Zmd000216'].size),
+                     Zp1       = slice(0,cropset['Zmd000216'].size+1),
+                     Zu        = slice(0,cropset['Zmd000216'].size))
+        for dim in ['X', 'Xp1', 'Y', 'Yp1']: cropset[dim]=ds[dim]
+        ds = xr.merge([ds, cropset])
+        
+    # Adjust dimensions creating conflicts
+    ds = ds.rename({'Z': 'Ztmp'})
+    ds = ds.rename({'T': 'time', 'Ztmp': 'Z', 'Zmd000216': 'Z'})
+    ds = ds.squeeze('Zd000001')
+    
+    for dim in ['Z','Zp1', 'Zu','Zl']:
+        ds[dim].values   = ds[dim].values
+        ds[dim].attrs.update({'positive': 'up'}) 
+    
+    # Add xgcm info
+    for dim in ['Z', 'X', 'Y']: ds[dim].attrs.update({'axis': dim})  
+    for dim in ['Zp1','Zu','Zl','Xp1','Yp1']: 
+        if min(ds[dim].values)<min(ds[dim[0]].values):
+            ds[dim].attrs.update({'axis': dim[0], 'c_grid_axis_shift': -0.5})
+        elif min(ds[dim].values)>min(ds[dim[0]].values):
+            ds[dim].attrs.update({'axis': dim[0], 'c_grid_axis_shift': +0.5})
+    
+    # ByeBye
+    elapsed_time = time.time() - start_time
+    print(time.strftime('done in %H:%M:%S', time.gmtime(elapsed_time)))
+        
+    return ds
+
