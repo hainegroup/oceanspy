c56a85de1eea9f8b40a8191ab349db63f40e7109
Mattia Almansi <mattia.almansi@jhu.edu>
2018-05-21 22:13:06 -0400

    Update test_oceanspy.py

--- oceanspy/tests/test_oceanspy.py
+++ oceanspy/tests/test_oceanspy.py
@@ -4,23 +4,23 @@
 """Tests for `oceanspy` package."""
 
 import pytest
+import oceanspy as ospy
 
-"""
-from oceanspy import oceanspy
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#from oceanspy import oceanspy
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-"""
+#@pytest.fixture
+#def response():
+#    """Sample pytest fixture.
+#
+#    See more at: http://doc.pytest.org/en/latest/fixture.html
+#    """
+#    # import requests
+#    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#
+#
+#def test_content(response):
+#    """Sample pytest test function with the pytest fixture as an argument."""
+#    # from bs4 import BeautifulSoup
+#    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
