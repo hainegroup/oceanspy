58f006095e4e47ef3d0fcdbfdad7785a93f58398
malmans2 <mattia.almansi@jhu.edu>
2019-02-08 15:26:18 +0000

    remove old modules

--- oceanspy/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = """Mattia Almansi"""
-__email__ = 'mattia.almansi@jhu.edu'
-__version__ = '0.0.10'
-
-from . import open_dataset
-from . import subsample
-from . import utils
-from . import compute
-from . import visualize
