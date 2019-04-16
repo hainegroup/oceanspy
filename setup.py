8580b01a93b4e3e771b72f926dea395157cdd786
Mattia Almansi <mattia.almansi@jhu.edu>
2019-04-16 11:01:19 +0200

    test 36 and 37

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description=("OceanSpy: A Python package for"
                  " easy ocean model data analysis and visualization"),
