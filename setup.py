ebdf1bcb28f1d4c6d39428e8116f87684d9d7718
malmans2 <mattia.almansi@jhu.edu>
2019-04-20 19:06:39 +0000

    fix catalogs

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
     keywords='oceanspy',
     name='oceanspy',
-    packages=find_packages(include=['oceanspy']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
