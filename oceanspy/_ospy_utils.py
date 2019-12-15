800b76eff2d1cb61ddaa73eab82526690a52c87f
Miguel Jimenez-Urias <mjimen17@jh.edu>
2019-12-15 18:24:40 -0500

    added functionality to make POP output compatible with xgcm

--- oceanspy/_ospy_utils.py
+++ oceanspy/_ospy_utils.py
@@ -346,6 +346,121 @@ def _restore_coord_attrs(ds):
             ds[var].attrs['coordinates'] = coordinates
     return ds
 
+# ========
+# POP Model Output
+# ========
+
+def _add_pop_dims_to_dataset(ds):
+    ds_new = ds.copy()
+    ds_new['nlon_u'] = xr.Variable(('nlon_u'), np.arange(len(ds.nlon)) + 1, {'axis': 'X', 'c_grid_axis_shift': 0.5})
+    ds_new['nlat_u'] = xr.Variable(('nlat_u'), np.arange(len(ds.nlat)) + 1, {'axis': 'Y', 'c_grid_axis_shift': 0.5})
+    ds_new['nlon_t'] = xr.Variable(('nlon_t'), np.arange(len(ds.nlon)) + 0.5, {'axis': 'X'})
+    ds_new['nlat_t'] = xr.Variable(('nlat_t'), np.arange(len(ds.nlat)) + 0.5, {'axis': 'Y'})
+
+    # add metadata to z grid
+    ds_new=ds_new.set_coords({'z_t','z_w','z_w_top','z_w_bot'})
+    ds_new['z_t'].attrs.update({'axis': 'Z'})
+    ds_new['z_w'].attrs.update({'axis': 'Z', 'c_grid_axis_shift': -0.5})
+    ds_new['z_w_top'].attrs.update({'axis': 'Z', 'c_grid_axis_shift': -0.5})
+    ds_new['z_w_bot'].attrs.update({'axis': 'Z', 'c_grid_axis_shift': 0.5})
+
+    return ds_new
+
+
+def _dims_from_grid_loc(grid_loc):
+    grid_loc = str(grid_loc)
+    ndim = int(grid_loc[0])
+    x_loc_key = int(grid_loc[1])
+    y_loc_key = int(grid_loc[2])
+    z_loc_key = int(grid_loc[3])
+
+    x_loc = {1: 'nlon_t', 2: 'nlon_u'}[x_loc_key]
+    y_loc = {1: 'nlat_t', 2: 'nlat_u'}[y_loc_key]
+    z_loc = {0: 'surface', 1: 'z_t', 2: 'z_w', 3: 'z_w_bot', 4: 'z_t_150m'}[z_loc_key]
+
+    if ndim == 2: # meant to represent spatial coordinates (not time)
+        return y_loc, x_loc
+    else: # ndim >= 3
+        return z_loc, y_loc, x_loc
+
+
+def _label_coord_grid_locs(ds):
+    ''' Assign grid loc to label coordinates for each variables. grid_loc consists of 4 numbers grid_loc=abcd, where
+        a: ndim
+        b: x_loc
+        c: y_loc
+        d: z_loc
+    Examples:
+        2D variables:
+            2220: nlat_u, nlon_u
+            2210: nlat_t, nlon_u
+            2120: nlat_u, nlon_t
+            2110: nlat_t, nlon_t
+        3D variables
+            3221: z_t,nlat_u,nlon_u
+            3
+    '''
+    grid_locs = {'ANGLE': '2220', 'ANGLET': '2110',
+                 'DXT': '2110', 'DXU': '2220',
+                 'DYT': '2110', 'DYU': '2220',
+                 'HT': '2110', 'HU': '2220',
+                 'HTE': '2210', 'HTN': '2120',
+                 'HUS': '2210', 'HUW': '2120',
+                 'KMT': '2110', 'KMU': '2220',
+                 'REGION_MASK': '2110',
+                 'TAREA': '2110', 'TLAT': '2110', 'TLONG': '2110',
+                 'UAREA': '2220', 'ULAT': '2220', 'ULONG': '2220',
+                 'ADVU': '4221', 'ADVV': '4221',
+                 'DIA_IMPVF_SALT': '4112', 'DIA_IMPVF_TEMP': '4112',
+                 'EVAP_F': '2110', 'GRADX': '4221', 'GRADY': '4221',
+                 'HBLT': '2110', 'HMXL': '2110',
+                 'HDIFB_SALT': '4112', 'HDIFB_TEMP': '4112',
+                 'HDIFE_SALT': '4211', 'HDIFE_TEMP': '4211',
+                 'HDIFN_SALT': '4121', 'HDIFN_TEMP': '4121',
+                 'HDIFFU': '4221', 'HDIFFV': '4221',
+                 'KPP_SRC_SALT': '4111', 'KPP_SRC_TEMP': '4111',
+                 'LWDN_F': '2110', 'LWUP_F': '2110',
+                 'MELTH_F': '2110', 'MELT_F': '2110',
+                 'PD': '4111', 'PREC_F': '2110', 'QFLUX': '2110',
+                 'QSW_3D': '4111', 'ROFF_F': '2110', 'SALT': '4111',
+                 'SALT_F': '2110', 'SENH_F': '2110', 'SFWF': '2110',
+                 'SFWF_WRST': '2110', 'SHF': '2110', 'SHF_QSW': '2110',
+                 'SNOW_F': '2110', 'SSH': '2110', 'SSH2': '2110',
+                 'SU': '2220', 'SV': '2220', 'TAUX': '2220', 'TAUY': '2220',
+                 'TBLT': '2110', 'TEMP': '4111', 'TEND_TEMP': '4111',
+                 'TEND_SALT': '4111', 'TMXL': '2110',
+                 'UES': '4211', 'UET': '4211', 'UV': '4221',
+                 'UVEL': '4221', 'UVEL2': '4221',
+                 'VVEL': '4221', 'VVEL2': '4221',
+                 'VDIFFU': '4221', 'VDIFFV': '4221',
+                 'VNS': '4121', 'VNT': '4121',
+                 'WTS': '4112', 'WTT': '4112', 'WVEL': '4222',
+                 'XBLT': '2110', 'XMXL': '2110'}
+    ds_new = ds.copy()
+    for var in ds_new.variables:
+        if var in grid_locs.keys():
+            ds_new[var].attrs['grid_loc'] = grid_locs[var]
+    return ds_new
+
+
+def _relabel_pop_dims(ds):
+    """Return a new xarray dataset with distinct dimensions for variables at
+    different grid points.
+    """
+    ds_new = _label_coord_grid_locs(ds)
+    ds_new = _add_pop_dims_to_dataset(ds_new)
+    for vname in ds_new.variables:
+        if 'grid_loc' in ds_new[vname].attrs:
+            da = ds_new[vname]
+            dims_orig = da.dims
+            new_spatial_dims = _dims_from_grid_loc(da.attrs['grid_loc'])
+            if dims_orig[0] == 'time':
+                dims = ('time',) + new_spatial_dims
+            else:
+                dims = new_spatial_dims
+            ds_new[vname] = xr.Variable(dims, da.data, da.attrs, da.encoding, fastpath=True)
+    return ds_new
+
 
 # ========
 # MESSAGES
