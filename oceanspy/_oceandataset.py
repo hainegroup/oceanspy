445fcfda2674b4228295cd404be3d8a34efcd19d
Miguel Jimenez-Urias <mjimen17@jh.edu>
2019-12-20 04:16:24 -0500

    remove testing

--- oceanspy/_oceandataset.py
+++ oceanspy/_oceandataset.py
@@ -614,10 +614,7 @@ class OceanDataset:
 
         for k in range(13):
             for axis in ['X', 'Y']:
-                if type(face_connections['face'][k][axis])==tuple:
-                    print('')
-                else:
-                    face_connections['face'][k][axis]=eval(face_connections['face'][k][axis])
+                face_connections['face'][k][axis]=eval(face_connections['face'][k][axis])
 
         self = self._store_as_global_attr(name='face_connections',
                                           attr=face_connections,
