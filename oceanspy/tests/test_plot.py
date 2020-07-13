ef69b6b2707caad51acf2ccd7046684e90b5c248
malmans2 <mattia.almansi@jhu.edu>
2020-07-13 13:31:58 +0100

    fix compatibility with xarray v0.16

--- oceanspy/tests/test_plot.py
+++ oceanspy/tests/test_plot.py
@@ -209,24 +209,24 @@ def test_hor_sec_warn(od_in):
 
     with pytest.warns(UserWarning):
         plt.close()
-        fig, ax = plt.subplots(1, 1)
+        # fig, ax = plt.subplots(1, 1)
         ax = horizontal_section(
             od_in,
             varName="Eta",
             contourName="Depth",
-            ax=ax,
+            # ax=ax,
             subplot_kws={"projection": None},
         )
         assert isinstance(ax, xr.plot.FacetGrid)
 
     with pytest.warns(UserWarning):
         plt.close()
-        fig, ax = plt.subplots(1, 1)
+        # fig, ax = plt.subplots(1, 1)
         ax = horizontal_section(
             od_in,
             varName="Eta",
             contourName="V",
-            ax=ax,
+            # ax=ax,
             meanAxes=["Z"],
             subplot_kws={"projection": od.projection},
         )
