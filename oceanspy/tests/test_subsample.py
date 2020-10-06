8e8f4e069e592b293e4452a01bc2cebe1e084824
Miguel Jimenez-Urias <mjimen17@jh.edu>
2020-10-05 20:03:17 -0400

    entries in parametrize

--- oceanspy/tests/test_subsample.py
+++ oceanspy/tests/test_subsample.py
@@ -261,12 +261,15 @@ def test_reduce_variables(od, varList):
 
 @pytest.mark.parametrize("od", [ECCOod])
 @pytest.mark.parametrize(
-    "XRange, YRange, ZRange, varList, transformation, centering", [
-        ([-90, 20], [20, 80], None, ['T'], 'arctic_crown', 'Atlantic'),
+    "XRange, YRange, ZRange, varList, transformation, centering, NZ", [
+        ([-90, 20], [20, 80], None, ['T'], 'arctic_crown', 'Atlantic', 90),
+        ([-179, 179], [20, 80], None, ['U', 'V'], 'arctic_crown',
+                                                  'Atlantic', 90),
+        ([-179, 179], [55, 90], -100, ['T'], 'arctic_centered', 'Pacific', 1),
     ],
 )
 def test_cutout_faces(od, XRange, YRange, ZRange, varList, transformation,
-                      centering):
+                      centering, NZ):
     args = {
         "od": od,
         "varList": varList,
@@ -278,6 +281,7 @@ def test_cutout_faces(od, XRange, YRange, ZRange, varList, transformation,
     }
     new_od = od.subsample.cutout(**args)
     assert (set(od.dataset.dims) - set(new_od.dataset.dims)) == set(['face'])
+    assert new_od._ds.dims['Z'] == NZ
 
 
 # =======
