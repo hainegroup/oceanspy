3a4d5b8d1d0ba50018046bd3d565739ef15a7d5e
malmans2 <mattia.almansi@jhu.edu>
2021-03-02 10:41:01 +0000

    deprecate 3.6

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description=(
         "OceanSpy: A Python package to"
