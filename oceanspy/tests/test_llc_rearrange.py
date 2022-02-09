601856d3fa0309cfff523d23c5f68f15c7467d0f
Miguel Jimenez <mjimen17@jhu.edu>
2022-02-09 16:01:26 -0500

    add test for transformation

--- oceanspy/tests/test_llc_rearrange.py
+++ oceanspy/tests/test_llc_rearrange.py
@@ -73,12 +73,88 @@ acshape = (Nx // 2, Ny)
         (od, faces[6:], [0, 0, 7, 10], int),
     ],
 )
-def test_arc_connect(od, faces, expected, acshape):
+def test_arc_connect(od, faces, expected, atype):
     ds = od._ds
     arc_faces, *a, DS = arct_connect(ds, "XG", faces)
     assert arc_faces == expected
     if len(DS) > 0:
         assert type(DS[0]) == atype
+
+
+transf = "arctic_crown"
+cent = ["Atlantic", "Pacific"]
+varlist = ["T", "U", "V", "XG", "YG"]
+
+@pytest.mark.parametrize(
+    "od, faces, varlist, transf, centered, drop, X0, X1, Y0, Y1",
+    [
+        (od, 'all', varlist, transf, cent[0], False, 0, 359, 0, 314),
+        (od, [2, 5, 6, 7, 10], varlist, transf, cent[0], False, 0, 359, 180, 314),
+        (od, [2, 5, 7, 10], varlist, transf, cent[0], False, 0, 359, 180, 269),
+        (od, [1, 4, 8, 11], varlist, transf, cent[0], False, 0, 359, 90, 179),
+        (od, [0, 3, 9, 12], varlist, transf, cent[0], False, 0, 359, 0, 89),
+        (od, [6, 7, 8, 9], varlist, transf, cent[0], False, 0, 89, 314, 0),
+        (od, [6, 10, 11, 12], varlist, transf, cent[0], False, 90, 179, 314, 0),
+        (od, [0, 1, 2, 6], varlist, transf, cent[0], False, 180, 269, 0, 314),
+        (od, [3, 4, 5, 6], varlist, transf, cent[0], False, 270, 359, 0, 314),
+        (od, [2, 6, 10], varlist, transf, cent[0], False, 90, 269, 180, 314),
+        (od, [6, 7, 10], varlist, transf, cent[0], False, 0, 179, 314, 180),
+        (od, [5, 6, 7], varlist, transf, cent[1], False, 90, 269, 180, 314),
+        (od, [5, 6, 7, 10], varlist, transf, cent[1], False, 90, 359, 180, 314),
+        (od, [6, 7, 8, 10, 11], varlist, transf, cent[0], False, 0, 179, 314, 90),
+        (od, [4, 5, 6, 7, 8, 10, 11], varlist, transf, cent[1], False, 90, 359, 90, 314),
+        (od, [4, 5, 6, 7, 8, 9, 10, 11, 12], varlist, transf, cent[1], False, 90, 359, 0, 314),
+        (od, [4, 5, 6, 7, 8], varlist, transf, cent[1], False, 90, 269, 90, 314),
+        (od, [0, 3, 9, 12], varlist, transf, cent[1], False, 0, 359, 0, 89),
+        (od, [1, 4, 8, 11], varlist, transf, cent[1], False, 0, 359, 90, 179),
+        (od, [7, 8, 10, 11], varlist, transf, cent[0], False, 0, 179, 269, 90),
+        (od, [1, 2, 10, 11], varlist, transf, cent[0], False, 90, 269, 90, 269),
+        (od, [8, 9, 11, 12], varlist, transf, cent[0], False, 0, 179, 179, 0),
+        (od, [0, 1, 11, 12], varlist, transf, cent[0], False, 90, 269, 0, 179),
+        (od, [0, 1, 3, 4], varlist, transf, cent[0], False, 180, 359, 0, 179),
+        (od, [9, 12], varlist, transf, cent[0], False, 0, 179, 89, 0),
+        (od, [0, 12], varlist, transf, cent[0], False, 90, 269, 0, 89),
+        (od, [0, 3], varlist, transf, cent[0], False, 180, 359, 0, 89),
+        (od, [3, 9], varlist, transf, cent[1], False, 90, 269, 0, 89),
+        (od, [0, 9, 12], varlist, transf, cent[0], False, 0, 269, 0, 89),
+        (od, [0, 3, 12], varlist, transf, cent[0], False, 90, 359, 0, 89),
+        (od, [0, 3, 9], varlist, transf, cent[1], False, 0, 269, 0, 89),        
+        (od, [0], varlist[0], transf, cent[0], False, 180, 269, 0, 89),
+        (od, [1], varlist[0], transf, cent[0], False, 180, 269, 90, 179),
+        (od, [2], varlist[0], transf, cent[0], False, 180, 269, 180, 269),
+        (od, [3], varlist[0], transf, cent[0], False, 270, 359, 0, 89),
+        (od, [4], varlist[0], transf, cent[0], False, 270, 359, 90, 179),
+        (od, [5], varlist[0], transf, cent[0], False, 270, 359, 180, 269),
+        (od, [7], varlist[0], transf, cent[0], False, 0, 89, 269, 180),
+        (od, [8], varlist[0], transf, cent[0], False, 0, 89, 179, 90),
+        (od, [9], varlist[0], transf, cent[0], False, 0, 89, 89, 0),
+        (od, [10], varlist[0], transf, cent[0], False, 90, 179, 269, 180),
+        (od, [11], varlist[0], transf, cent[0], False, 90, 179, 179, 90),
+        (od, [12], varlist[0], transf, cent[0], False, 90, 179, 89, 0),
+    ]
+)
+
+
+def test_transformation(od, faces, varlist, transf, centered, drop, X0, X1, Y0, Y1):
+    ds = od._ds.reset_coords()
+    args = {
+        "ds": ds,
+        "varlist": varlist,
+        "centered": centered,
+        "faces": faces,
+        "drop": drop,
+    }
+    if transf == "arctic_crown":
+        _transf = LLC.arctic_crown
+    elif transf == "arctic_centered":
+        _transf = LLC.arctic_centered
+    ds = _transf(**args)
+    xi, xf = int(ds["X"][0].values), int(ds["X"][-1].values)
+    yi, yf = int(ds["Y"][0].values), int(ds["Y"][-1].values)
+    assert xi == X0
+    assert xf == X1
+    assert yi == Y0
+    assert yf == Y1
 
 
 @pytest.mark.parametrize(
