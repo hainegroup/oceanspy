72d83d74b45f8a36ed3ca1758fb664473b8cd757
Ali Hasan Siddiqui <43628137+asiddi24@users.noreply.github.com>
2022-02-16 23:14:51 -0500

    python

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
+    python_requires=">=3.7,<3.10",
     url="https://github.com/hainegroup/oceanspy",
     # fmt: off
     version='0.2.0',
