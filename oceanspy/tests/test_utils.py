563101a72363cf6aa4f67050e1771b5544a9df7e
Miguel Jimenez <mjimen17@jhu.edu>
2022-10-18 14:01:49 -0400

    write tests for rel_lon

--- oceanspy/tests/test_utils.py
+++ oceanspy/tests/test_utils.py
@@ -2,7 +2,7 @@
 import pytest
 
 # From OceanSpy
-from oceanspy.utils import cartesian_path, great_circle_path, spherical2cartesian
+from oceanspy.utils import cartesian_path, great_circle_path, spherical2cartesian, _rel_lon
 
 
 def test_RNone():
@@ -21,3 +21,30 @@ def test_error_path():
 
     with pytest.raises(ValueError):
         cartesian_path(1, 2, 3, 4, delta=-1)
+
+
+X0 = _np.array([161, -161]) # track begins west, ends east
+X1 = list(_np.arange(161,180, 2)) + list(_np.arange(-179, -159, 2)) # same as above
+X1 = _np.array(X1)
+X2 = list(_np.arange(-179, -159, 2))[::-1] + list(_np.arange(161,180, 2))[::-1]
+X2 = _np.array(X2)  # track begins east, ends west.
+X3 = _np.array([-20, 20])  # crossing in zero.
+X3 = _np.array([-20, -1])  # no crossing.
+
+@pytest.mark.parametrize(
+    "XRange, expected",
+    [
+    (X0, 53.66),
+    (X1, 53.66), 
+    (X2, 53.66),
+    (X3, 180),
+    (X4, 180),
+    ]
+)
+
+def test_rel_lon(XRange, expected):
+    """ test the function rel_lon which redefines the reference long.
+    """
+    ref_lon = _rel_lon(XRange)
+    assert ref_lon == expected
+
