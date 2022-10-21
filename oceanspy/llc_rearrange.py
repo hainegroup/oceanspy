47739e421a258f3c6292f6f71dcec063bcb008d5
Miguel Jimenez <mjimen17@jhu.edu>
2022-10-20 20:52:50 -0400

    fixed bug!!!!

--- oceanspy/llc_rearrange.py
+++ oceanspy/llc_rearrange.py
@@ -792,7 +792,7 @@ def _edge_arc_data(_da, _face_ind, _dims):
     return X0
 
 
-def mask_var(_ds, XRange=None, YRange=None, _faces='all', ref_lon=180):
+def mask_var(_ds, XRange=None, YRange=None, ref_lon=180):
     """Returns a dataset with masked latitude at C and G points (YC and YG).
     The masking region is determined by XRange and YRange. Used to estimate the
     extend of actual data to be retained.
