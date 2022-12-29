5908b233ba7703880fe37920c4895fac3dc23c50
Miguel Jimenez <mjimen17@jhu.edu>
2022-12-28 19:20:04 -0800

    remove sign change

--- oceanspy/llc_rearrange.py
+++ oceanspy/llc_rearrange.py
@@ -498,8 +498,6 @@ def arct_connect(ds, varName, faces=None, masking=False, opt=False, ranges=None)
                 if len(dims.X) + len(dims.Y) == 4:
                     if len(dims.Y) == 1 and _varName not in metrics:
                         fac = -1
-                elif _varName == "SN":
-                    fac = -1
                 da_arg = {"face": arc_cap, dims.X: xslice, dims.Y: yslice}
                 mask_arg = {dims.X: xslice, dims.Y: yslice}
                 arct = fac * ds[_varName].isel(**da_arg)
@@ -535,8 +533,6 @@ def arct_connect(ds, varName, faces=None, masking=False, opt=False, ranges=None)
                 Ny_ac_rot.append(0)
                 da_arg = {"face": arc_cap, dims.X: xslice, dims.Y: yslice}
                 mask_arg = {dims.X: xslice, dims.Y: yslice}
-                if _varName == 'CS':
-                    fac = -1
                 arct = fac * ds[_varName].isel(**da_arg)
                 Mask = mask7.isel(**mask_arg)
                 if opt:
