47b616141712bfccf36c9ed40bc914b873a4c8aa
Miguel Jimenez <mjimen17@jhu.edu>
2022-12-28 18:08:22 -0800

    sine correction

--- oceanspy/llc_rearrange.py
+++ oceanspy/llc_rearrange.py
@@ -569,7 +569,7 @@ def arct_connect(ds, varName, faces=None, masking=False, opt=False, ranges=None)
                 if len(dims.X) + len(dims.Y) == 4:
                     if len(dims.Y) == 1 and _varName not in metrics:
                         fac = -1
-                elif _varName == "CS":
+                elif _varName == "SN":
                     fac = -1
                 da_arg = {"face": arc_cap, dims.X: xslice, dims.Y: yslice}
                 mask_arg = {dims.X: xslice, dims.Y: yslice}
