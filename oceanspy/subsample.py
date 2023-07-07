457ce39904273e07c3cd1f3e0da23a87725a6f5c
Miguel Jimenez <mjimen17@jhu.edu>
2023-07-06 22:50:59 -0400

    do not remove X Y from grid coords

--- oceanspy/subsample.py
+++ oceanspy/subsample.py
@@ -1310,8 +1310,8 @@ def stations(
             od = od.set_face_connections(**new_face_connections)
 
         grid_coords = od.grid_coords
-        grid_coords.pop("X", None)
-        grid_coords.pop("Y", None)
+        # grid_coords.pop("X", None)
+        # grid_coords.pop("Y", None)
         od = od.set_grid_coords(grid_coords, overwrite=True)
 
     return od
