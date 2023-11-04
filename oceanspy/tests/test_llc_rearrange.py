0483e85dc95a8340bae4371b8d349b301d2d66d9
Miguel Jimenez <mjimen17@jhu.edu>
2023-11-03 20:47:33 -0700

    improve test

--- oceanspy/tests/test_llc_rearrange.py
+++ oceanspy/tests/test_llc_rearrange.py
@@ -2934,7 +2934,7 @@ def test_mates(dataset, pairs):
         (10, 7, 2),
         (10, 2, 3),
         (0, 1, 3),
-        (0, 3, None),
+        (0, 8, None),
     ],
 )
 def test_face_direction(od, face1, face2, value):
